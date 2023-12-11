f28abe11ca1691988ca657812b90524cbbf40015
Abhishek Sethi <143688066+AbhishekSethi2005@users.noreply.github.com>
2023-12-11 08:18:58 +0530

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### My First Program
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print("Abhishek Sethi")
+
+
+# In[1]:
+
+
+print("Sanjivani")
+
+
+# ## Data type
+
+# In[3]:
+
+
+name="Latha"
+type(name) 
+
+
+# In[5]:
+
+
+X=10
+type(X)
+
+
+# In[6]:
+
+
+x=10.5
+type(x)
+
+
+# In[7]:
+
+
+x=True
+type(x)
+
+
+# ## Variables
+
+# In[8]:
+
+
+name="Abhishek"
+name
+
+
+# In[9]:
+
+
+name lists="Abhishek","Sethi"
+name lists
+
+
+# ### variable name should not have space
+
+# In[10]:
+
+
+2names="Abhishek"
+
+
+# In[13]:
+
+
+na2mes="Abhishek"
+name
+
+
+# ### variable name should not start with number
+
+# In[14]:
+
+
+names_lists="Abhishek","Sethi"
+names_lists
+
+
+# In[15]:
+
+
+names+lists="Abhishek","Sethi"
+names+lists
+
+
+# ### The only allowed special character is underscore
+
+# In[16]:
+
+
+_name="Abhishek"
+_name
+
+
+# ### Variable names can start with A-Z , a-z , _
+
+# # List
+
+# In[18]:
+
+
+sample_list=[1,2,3,4,0,-1,1,1.2,'hi']
+sample_list
+
+
+# In[19]:
+
+
+type(sample_list)
+
+
+# In[20]:
+
+
+sample_list[0]
+
+
+# In[21]:
+
+
+sample_list[10]
+
+
+# In[22]:
+
+
+sample_list[-3]
+
+
+# In[23]:
+
+
+sample_list[0]=100
+sample_list
+
+
+# ### Square brackets
+# ### elements can be retrived using index
+# ### mutable--> can change
+# ### heterogenous--> multiple data types
+
+# # Tuples
+
+# In[2]:
+
+
+sample_tuple=(1,2,3,4,5)
+sample_tuple[0]
+
+
+# In[3]:
+
+
+sample_tuple[-1]
+
+
+# In[4]:
+
+
+sample_tuple[0]=100
+
+
+# ### Tuple is immutable -> cannot make any changes
+# ### can write with () or no brackets
+
+# # Dictionary
+
+# In[6]:
+
+
+dict_1={'name':"arun","hobbies":["paintaing","singing"],5:2.3}
+type(dict_1)
+
+
+# In[7]:
+
+
+dict_1
+
+
+# In[8]:
+
+
+dict_1.keys()
+
+
+# In[9]:
+
+
+dict_1.values()
+
+
+# In[10]:
+
+
+dict_1['hobbies']
+
+
+# In[12]:
+
+
+dict_1["name"]="Abhishek"
+
+
+# In[13]:
+
+
+dict_1
+
+
+# ### Dictionary has Key-Value pair Data structure(DS)
+# ### We can rretrieve values using index
+# ### Mutable
+# ### {} are allowed
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
